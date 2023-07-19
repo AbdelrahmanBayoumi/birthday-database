9812fdcadd78804346b3e5d59b98a0afa56464b7
Abdelrahman Bayoumi <abdelrahmanbayoumi1@gmail.com>
2023-07-19 03:43:25 +0300

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,6 +2,11 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
+  @Get()
+  getHello(): string {
+    return 'Hello World!';
+  }
+
   @Get('/health-check')
   helthCheck(): string {
     return "I'm alive!";
