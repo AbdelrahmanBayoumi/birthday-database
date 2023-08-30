a7cff5d50bbf5cc8dbc1acebd521b9b7f4e19d57
Abdelrahman Bayoumi <abdelrahmanbayoumi1@gmail.com>
2023-08-30 17:34:42 +0300

    Fix `/` html title

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -12,6 +12,6 @@ export class AppController {
   @ApiOkResponse({ description: 'Hello World endpoint' })
   @Get()
   getHello(): string {
-    return `<!DOCTYPE html> <html> <head> <title>Email Activation</title> </head> <body style=" background-color: #f5f7fe; text-align: center; " > <div style=" height: 100vh; display: flex; align-items: center; justify-content: center; " > <div style=" padding: 40px; display: flex; flex-direction: column; background: #fff; border-radius: 20px; box-shadow: 2px 3px 10px #00000029; " > <h1>📅 Birthday Database</h1> <h2> ✅ go to <a href='/api'>/api</a> for docs. </h2>  </div> </div> </body> </html>`;
+    return `<!DOCTYPE html> <html> <head> <title>Birthday Database</title> </head> <body style=" background-color: #f5f7fe; text-align: center; " > <div style=" height: 100vh; display: flex; align-items: center; justify-content: center; " > <div style=" padding: 40px; display: flex; flex-direction: column; background: #fff; border-radius: 20px; box-shadow: 2px 3px 10px #00000029; " > <h1>📅 Birthday Database</h1> <h2> ✅ go to <a href='/api'>/api</a> for docs. </h2>  </div> </div> </body> </html>`;
   }
 }
